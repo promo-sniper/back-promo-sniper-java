74fd6b6817c2953b6940e925cf24fee76adb9f6b
anorneto <anornetoo@gmail.com>
2023-12-23 19:20:04 -0300

    wip: project structure

--- src/main/java/com/anorneto/promosniper/PromoSniperApplication.java
+++ src/main/java/com/anorneto/promosniper/PromoSniperApplication.java
@@ -1,6 +1,9 @@
 package com.anorneto.promosniper;
 
 import com.anorneto.promosniper.presenters.controller.UserController;
+import com.anorneto.promosniper.presenters.helpers.AppHealthCheck;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
@@ -19,12 +22,18 @@ public class PromoSniperApplication extends Application<PromoSniperConfiguration
     @Override
     public void initialize(final Bootstrap<PromoSniperConfiguration> bootstrap) {
         // TODO: application initialization
+        ObjectMapper jackssonObjectMapper = bootstrap.getObjectMapper();
+        jackssonObjectMapper.enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     }
 
     @Override
-    public void run(final PromoSniperConfiguration configuration,
-                    final Environment environment) {
+    public void run(
+            final PromoSniperConfiguration configuration,
+            final Environment environment
+    ) {
         environment.jersey().register(UserController.class);
+        AppHealthCheck appHealthCheck = new AppHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("appHealthCheck", appHealthCheck);
     }
 
 }
--- src/main/java/com/anorneto/promosniper/PromoSniperConfiguration.java
+++ src/main/java/com/anorneto/promosniper/PromoSniperConfiguration.java
@@ -1,10 +1,8 @@
 package com.anorneto.promosniper;
 
-import io.dropwizard.core.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.core.Configuration;
 import jakarta.validation.constraints.NotEmpty;
-import org.hibernate.validator.constraints.*;
-import jakarta.validation.constraints.*;
 
 public class PromoSniperConfiguration extends Configuration {
     @NotEmpty
