d244f045deaa3ffec7379568df8737a93b176739
Anor Neto <anornetoo@gmail.com>
2024-01-14 03:04:37 -0300

    feat: add cors filter

--- src/main/java/com/anorneto/promosniper/PromoSniperApplication.java
+++ src/main/java/com/anorneto/promosniper/PromoSniperApplication.java
@@ -10,12 +10,16 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterRegistration;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import java.util.EnumSet;
 
 /**
  * import io.federecio.dropwizard.swagger.SwaggerBundle;
  * import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
  */
-
 public class PromoSniperApplication extends Application<PromoSniperConfiguration> {
 
     public static void main(final String[] args) throws Exception {
@@ -37,17 +41,28 @@ public class PromoSniperApplication extends Application<PromoSniperConfiguration
         jackssonObjectMapper.registerModule(new JavaTimeModule());
 
         /**
-         // Swagger Initialization
-         bootstrap.addBundle(new SwaggerBundle<>() {
-        @Override protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(PromoSniperConfiguration configuration) {
-        return configuration.swaggerBundleConfiguration;
-        }
-        });
+         * // Swagger Initialization
+         * bootstrap.addBundle(new SwaggerBundle<>() {
+         * @Override protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(PromoSniperConfiguration configuration) {
+         * return configuration.swaggerBundleConfiguration;
+         * }
+         * });
          */
     }
 
     @Override
     public void run(final PromoSniperConfiguration configuration, final Environment environment) {
+        // Cors Filter
+        // environment.getApplicationContext().addFilter(CrossOriginFilter.class, "/*",
+        // EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR));
+        FilterRegistration.Dynamic corsFilter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,PATCH,POST,DELETE,OPTIONS");
+        corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        corsFilter.setInitParameter(
+                CrossOriginFilter.ALLOWED_HEADERS_PARAM,
+                "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        corsFilter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         // Register new routes here
         environment.jersey().register(UserController.class);
         environment.jersey().register(TelegramController.class);
